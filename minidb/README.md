621bc4a720aead5b021417557d0e1963f53e973b
Or Shemtov <oriesh@icloud.com>
2025-09-28 21:36:59 +0300

    Add REPL header file with statement definitions and structures

--- minidb/README.md
+++ minidb/README.md
@@ -15,3 +15,14 @@
 - WAL
 
 https://chatgpt.com/share/68d9557e-9d08-8009-a0af-b8ca9f586cb9
+
+- `CREATE TABLE name (colname TYPE, ...)`
+- `DROP TABLE name`
+- `TABLES (list tables)`
+- `CREATE INDEX idxname ON table(col) [UNIQUE]`
+- `DROP INDEX idxname`
+- `INSERT INTO table VALUES (val, ...)`
+- `SELECT * FROM table [WHERE col = value]`
+- `UPDATE table SET col=value [, ...] [WHERE col = value]`
+- `DELETE FROM table [WHERE col = value]`
+- `HELP, EXIT/QUIT`
---
+++ minidb/include/repl.h
@@ -0,0 +1,99 @@
+#ifndef REPL_H
+#define REPL_H
+
+#include "db.h"
+#include "row.h"
+#include <stdbool.h>
+
+typedef enum
+{
+    STMT_LIST_TABLES,
+    STMT_CREATE_TABLE,
+    STMT_DROP_TABLE,
+    STMT_INSERT,
+    STMT_SELECT,
+    STMT_DELETE,
+    STMT_UPDATE,
+    STMT_CREATE_INDEX,
+    STMT_DROP_INDEX,
+    STMT_HELP,
+    STMT_EXIT
+} StmtKind;
+
+typedef enum
+{
+    OP_EQ
+} PredOp;
+
+typedef struct
+{
+} WherePred;
+
+typedef struct
+{
+    const char* name;
+    MDBColumnDef cols[64];
+    uint16_t ncols;
+} StmtCreateTable;
+
+typedef struct
+{
+    const char* name;
+} StmtDropTable;
+
+typedef struct
+{
+    const char* name;
+    const char* table_name;
+    uint16_t col_idx;
+    bool is_unique;
+} StmtCreateIndex;
+
+typedef struct
+{
+    const char* name;
+} StmtDropIndex;
+
+typedef struct
+{
+    const char* table_name;
+    MDBValue values[128];
+    uint16_t nvalues;
+} StmtInsert;
+
+typedef struct
+{
+    const char* table_name;
+    WherePred where;
+} StmtSelect;
+
+typedef struct
+{
+    const char* table_name;
+    MDBValue values[128];
+    uint16_t nvalues;
+    WherePred where;
+} StmtUpdate;
+
+typedef struct
+{
+    const char* table_name;
+    WherePred where;
+} StmtDelete;
+
+typedef struct
+{
+    StmtKind kind;
+    union {
+        StmtCreateTable create_table;
+        StmtDropTable drop_table;
+        StmtCreateIndex create_index;
+        StmtDropIndex drop_index;
+        StmtInsert insert_;
+        StmtSelect select_;
+        StmtDelete delete_;
+        StmtUpdate update_;
+    };
+} Statement;
+
+#endif
